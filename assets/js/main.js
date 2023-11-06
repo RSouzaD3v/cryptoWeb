e9a9fa7d52823e6fa6c3742865ed41b980d79e95
Th3K1ngDev <rafaelf.souzan02@gmail.com>
2023-11-06 05:13:56 -0300

    segundo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,25 @@ var sectionCripto = document.querySelector('.criptoInfor');
 const connectionApi = async () => {
     try{
         fetch(url).then(res => res.json()).then(infor => {
-                        sectionCripto.innerHTML = 
+            const qtd = document.querySelector('.myMoney #money');
+            let btc = parseFloat(infor.data[0].vwap24Hr).toFixed(2);
+            let eth = parseFloat(infor.data[1].vwap24Hr).toFixed(2)     ;
+            let bnb = parseFloat(infor.data[2].vwap24Hr).toFixed(2);
+            let tether = parseFloat(infor.data[3].vwap24Hr).toFixed(2);
+            let dog = parseFloat(infor.data[8].vwap24Hr).toFixed(2);
+
+            qtd.addEventListener('change', (e) => {
+                let val = e.target.value;
+                let con = parseFloat(val)
+
+                if (Number.isNaN(con) == true){
+                    con = 1;
+                }
+                
+            
+    
+
+            sectionCripto.innerHTML = 
             `
             <section class="card">
                 <section class="iconMoeda">
@@ -12,8 +30,8 @@ const connectionApi = async () => {
                 </section>
                 <section class="information">
                     <h1>Name: Bitcoin</h1>
-                    <h1>Qtd: 1</h1>
-                    <h1>USD: ${infor.data[0].vwap24Hr}</h1>
+                    <h1>Qtd: ${con}</h1>
+                    <h1>USD: ${con * btc}</h1>
                 </section>
             </section>
             <section class="card">
@@ -22,8 +40,8 @@ const connectionApi = async () => {
                 </section>
                 <section class="information">
                     <h1>Name: Etherium</h1>
-                    <h1>Qtd: 1</h1>
-                    <h1>USD: ${infor.data[1].vwap24Hr}</h1>
+                    <h1>Qtd: ${con}</h1>
+                    <h1>USD: ${con * eth}</h1>
                 </section>
             </section>
             <section class="card">
@@ -32,8 +50,8 @@ const connectionApi = async () => {
                 </section>
                 <section class="information">
                     <h1>Name: Tether</h1>
-                    <h1>Qtd: 1</h1>
-                    <h1>USD: ${infor.data[2].vwap24Hr}</h1>
+                    <h1>Qtd: ${con}</h1>
+                    <h1>USD: ${con * tether}</h1>
                 </section>
             </section>
             <section class="card">
@@ -42,8 +60,8 @@ const connectionApi = async () => {
                 </section>
                 <section class="information">
                     <h1>Name: BNB</h1>
-                    <h1>Qtd: 1</h1>
-                    <h1>USD: ${infor.data[3].vwap24Hr}</h1>
+                    <h1>Qtd: ${con}</h1>
+                    <h1>USD: ${con * bnb}</h1>
                 </section>
             </section>
             <section class="card">
@@ -52,11 +70,13 @@ const connectionApi = async () => {
                 </section>
                 <section class="information">
                     <h1>Name: Dogecoin</h1>
-                    <h1>Qtd: 1</h1>
-                    <h1>USD: ${infor.data[8].vwap24Hr}</h1>
+                    <h1>Qtd: ${con}</h1>
+                    <h1>USD: ${con * dog}</h1>
                 </section>
             </section>`
         })
+    })
+
     }catch(error){
         //
     }
